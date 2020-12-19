e1154db66ffe776c5e2fc7a660389e48c848656d
maxfield-chen <maxfieldchen@gmail.com>
2020-12-19 11:42:31 -0800

    update newlines in install file.

--- install.sh
+++ install.sh
@@ -5,21 +5,21 @@
 # it, then you'll have a lot of paths to update. Your call.
 if ! hash docker &> /dev/null
 then
-    printf "Looks like you don't have docker, but that's no problem friend, I'll just go ahead and grab it for you. My treat, I insist."
+    printf "Looks like you don't have docker, but that's no problem friend, I'll just go ahead and grab it for you. My treat, I insist.\n"
     wget -O - https://get.docker.com/ | bash
 fi
 
 if ! hash python3 &> /dev/null
 then
-    printf "You don't seem to have python3 installed there partner. Go take a gander around https://www.python.org/. \n Now you come back real soon, you hear?"
+    printf "You don't seem to have python3 installed there partner. Go take a gander around https://www.python.org/. \n Now you come back real soon, you hear?\n"
     exit -1
 fi
 
 function getModel () {
-    printf "Fetching us a prebuilt model so we don't have to sit on our butts."
-    printf "That said, it's a quarter gig, so this may take a moment..."
+    printf "Fetching us a prebuilt model so we don't have to sit on our butts.\n"
+    printf "That said, it's a quarter gig, so this may take a moment...\n"
     wget "https://maxfieldchen.com/raw/im2txt_model_parameters.tar.gz"
-    printf "Done!, hopefully you're still with me."
+    printf "Done!, hopefully you're still with me.\n"
 }
 
 # If the model is not already here, download it.
@@ -27,10 +27,10 @@ function getModel () {
 
 # Builds docker image / container
 
-printf "Building the AI jail now..."
+printf "Building the AI jail now...\n"
 docker build . -t kalt && \
 docker build ./containers/ -t caption
-printf "AI's are secure. Probably."
+printf "AI's are secure. Probably.\n"
 
 # Linux by default, how about that...
 nativeManifestPath='/usr/lib/mozilla/native-messaging-hosts/'
@@ -50,7 +50,8 @@ cp ./run_inference_wrapper.py $nativeManifestPath
 # Set the path for the native file now that we know our platform
 python -c "import sys; lines = sys.stdin.read(); print lines.replace('FIXME', \"$nativeManifestPath/go.sh\")" < im2txt.json > "$nativeManifestPath$manifestName"
 
-
-printf "Install The manifest.json File In This Here Folder Right To Your Darn Did Browser."
+printf "=====================\n"
+printf "Install the manifest.json file to your browser.\n"
+printf "=====================\ n"
 
 exit 0
